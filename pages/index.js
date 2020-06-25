379c84163263739b91199b212ef9c6d921001e33
David Paul Crouch <david.crouch@hck2.com>
2020-06-25 15:24:03 -0500

    updated styling of wrappers

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
-export default () => <div>Hallo Wurld!</div>
+import { useState } from 'react'
+import SVG from '../components/SVG'
+
+var state = {}
+
+export default () => {
+  return (
+    <section>
+      <div className="colorBar"></div>
+      <header>
+        <SVG.logo/>
+      </header>
+      <div className="content"></div>
+    </section>
+  )
+}
